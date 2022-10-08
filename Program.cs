7005bd951cc1edbc4188abf89563cd01f47fe677
Simi <adegokesimi@gmail.com>
2022-10-08 19:03:30 +0100

    drop keys

--- Program.cs
+++ Program.cs
@@ -11,24 +11,17 @@ using BirdMessenger.Infrastructure;
 using BunnyCDN.Net.Storage;
 using Microsoft.Extensions.DependencyInjection;
 
-// var bunnyCDNStorage = new BunnyCDNStorage("smartstudy", "d43880ae-6577-4ead-8dda30522db1-7887-46e0", "de");
-// await bunnyCDNStorage.UploadAsync("sample_video.mp4", $"/smartstudy/{Guid.NewGuid().ToString("N")}.mp4");
-
-// var results = bunnyCDNStorage.GetStorageObjectsAsync("/smartstudy/");
-
-//var isUploaded = UploadVideoAsync("sample_video.mp4", "c92bcee6-28f8-4598-bdc6a4a9141b-c57c-48d0", "64975", Guid.NewGuid().ToString("N"));
-
 var baseUrl = "https://video.bunnycdn.com/library/";
 var client = new HttpClient();
-client.DefaultRequestHeaders.Add("AccessKey", "c92bcee6-28f8-4598-bdc6a4a9141b-c57c-48d0");
+client.DefaultRequestHeaders.Add("AccessKey", "access-key");
 var data = new StringContent("{\"title\":\"" + System.Web.HttpUtility.JavaScriptStringEncode(Guid.NewGuid().ToString("N")) + "\"}", Encoding.UTF8, "application/json");
-var reqCreate = await client.PostAsync(baseUrl + 64975 + "/videos", data);
+var reqCreate = await client.PostAsync(baseUrl + "videoID" + "/videos", data);
 var reqCreateResponse = JsonSerializer.Deserialize<Dictionary<String, Object>>(await reqCreate.Content.ReadAsStringAsync());
 if (reqCreate.StatusCode == HttpStatusCode.OK)
 {
     using (var stream = File.OpenRead("sample_video.mp4"))
     {
-        var reqUpload = await client.PutAsync(baseUrl + "64975" + "/videos/" + reqCreateResponse["guid"], new StreamContent(stream));
+        var reqUpload = await client.PutAsync(baseUrl + "videoID" + "/videos/" + reqCreateResponse["guid"], new StreamContent(stream));
         if (reqUpload.StatusCode == HttpStatusCode.OK)
         {
             Console.WriteLine(reqUpload.Content.ToString());
